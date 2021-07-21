f80893273e5eae3798ff14c729be182a19871034
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2021-07-21 16:11:07 +0300

    Fix query for find active surveys

--- src/main/java/fabrique/studio/test/task/repositories/SurveyRepository.java
+++ src/main/java/fabrique/studio/test/task/repositories/SurveyRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import fabrique.studio.test.task.models.Survey;
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
-    @Query("select s from Survey s where s.dateEnd >= CURRENT_DATE")
+    @Query("select s from Survey s where s.dateEnd >= CURRENT_DATE and s.dateStart <= CURRENT_DATE")
     List<Survey> findAllActiveSurveys();
     
     @Query("select distinct s from Survey s join s.questions q join q.answers a where a.extUserId = :extUserId")
